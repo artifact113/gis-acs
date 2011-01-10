c6374f82bb1c2b805c3191306138e617875c345a
fpiechocki <fpiechocki@04a8f1e0-413b-34ec-12da-15b1f2b27bf0>
2011-01-10 21:40:09 +0000

    Initial commit ;)

---
+++ graph.h
@@ -0,0 +1,69 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <QString>
+#include <QHash>
+
+namespace GIS {
+
+class Graph;
+class Edge;
+
+class Vertex
+{
+private:
+	Vertex(Graph *parentGraph, const QString &label);
+public:
+	Graph *graph() const;
+	QList<Vertex *> connectedVertices() const;
+	QString label() const;
+	Edge *edgeTo(Vertex *v) const;
+	Edge *connectTo(Vertex *v, int weight);
+	Edge *virtuallyConnectTo(Vertex *v, int weight);
+
+	friend class Graph;
+private:
+	QString m_label;
+	QHash<Vertex *, Edge *> m_connectedVertices;
+	Graph *m_graph;
+};
+
+class Edge
+{
+private:
+	Edge(Graph *parentGraph, Vertex *s, Vertex *e, int weight, bool virtual_ = false);
+public:
+	Vertex *startPoint() const;
+	Vertex *endPoint() const;
+	int weight() const;
+	bool isVirtual() const;
+
+	friend class Graph;
+	friend class Vertex;
+private:
+	Vertex *m_start;
+	Vertex *m_end;
+	int m_weight;
+	bool m_virtual;
+	Graph *m_graph;
+};
+
+class Graph
+{
+public:
+	Graph();
+	Vertex *createVertex(const QString &label);
+	Vertex *vertex(const QString &label) const;
+	QList<Vertex *> vertices() const;
+	bool isConnected() const;
+	bool readFromFile(const QString &filename);
+private:
+	void dfsTraverseFrom(Vertex *v) const;
+private:
+	QHash<QString, Vertex *> m_vertices;
+	mutable QList<Vertex *> m_visited;
+};
+
+} // namespace GIS
+
+#endif // GRAPH_H
