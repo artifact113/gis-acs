8563259f5d7457e508963de6125cf299da3599ab
fpiechocki <fpiechocki@04a8f1e0-413b-34ec-12da-15b1f2b27bf0>
2011-01-15 20:20:01 +0000

    dodane wczytwanie z pliku z lini polecen

--- main.cpp
+++ main.cpp
@@ -7,25 +7,38 @@ int main(int argc, char *argv[])
 	QApplication app(argc, argv);
 	QLabel label("GIS Project");
 
+	QString filename;
 	GIS::Graph *graph = new GIS::Graph;
-    qDebug() << "graph->readFromFile() returned:" << graph->readFromFile("c://test_short_paths.xml");
+	if (app.arguments().contains("-f")) {
+		int index = app.arguments().indexOf("-f");
+		if (index == -1) {
+			qDebug("'-f' but no '-f'...");
+			return 1;
+		}
+		if (app.arguments().size() <= ++index) {
+			qWarning("Wrong arguments list!");
+			return 1;
+		}
+		filename = app.arguments().at(index);
+	}
+	qDebug() << "graph->readFromFile() returned:" << graph->readFromFile(filename);
 	qDebug() << "graph->vertices().size() =" << graph->vertices().size();
 	label.showMaximized();
 
-        //graph->turnToCompleteGraph();
-        graph->findShortestPaths();
+	//graph->turnToCompleteGraph();
+	graph->findShortestPaths();
 
-        graph->printGraph();
+	graph->printGraph();
 
-        qDebug() << graph->vertex("A")->edgeTo(graph->vertex("E"))->weight();
-        QList<GIS::Vertex*> test = graph->getPath(graph->vertex("A"), graph->vertex("E"));
+	qDebug() << graph->vertex("A")->edgeTo(graph->vertex("E"))->weight();
+	QList<GIS::Vertex*> test = graph->getPath(graph->vertex("A"), graph->vertex("E"));
 
-        qDebug() << "rozmiar œcie¿ki: " << test.size();
+	qDebug() << "rozmiar sciezki: " << test.size();
 
-        foreach(GIS::Vertex* v, test)
-        {
-            qDebug() << v->label();
-        }
+	foreach(GIS::Vertex* v, test)
+	{
+		qDebug() << v->label();
+	}
 
 
 	return app.exec();
